fd7cdbb96572bbf3242035662d7f51852a0f2a42
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2025-03-29 11:37:25 +0100

    Laboratory Exercises

--- Labs/Lab01 Uninformed Search/Football.py
+++ Labs/Lab01 Uninformed Search/Football.py
@@ -119,4 +119,3 @@ if __name__ == '__main__':
         print(result.solution())
     else:
         print("No Solution!")
-
--- Labs/Lab01 Uninformed Search/Snake.py
+++ Labs/Lab01 Uninformed Search/Snake.py
@@ -184,4 +184,3 @@ if __name__ == '__main__':
         print(result.solution())
     else:
         print("No solution")
-
